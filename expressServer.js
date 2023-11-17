ee6b445714eeb10944c0da75d7d4fa6bdf0f6399
sidharth <sidharth.nair@neoito.com>
2023-11-17 13:14:41 +0530

    renamed variables nd read/write JSON file functions

--- expressServer.js
+++ expressServer.js
@@ -3,12 +3,10 @@ const fs = require("fs");
 const uuid = require("uuid");
 const server = express();
 const exampleJson = { name: "sid", age: "22" }; //example data for running tests
-let dataInJson; //for write/append operations
+let data; //for write/append operations
 
-//middleware functions
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-
+server.use(express.json()); //middleware
+server.use(express.urlencoded({ extended: true })); //middleware
 server.get("/", (req, res) => {
   res.end("Welcome to landing page, this is a test");
 });
@@ -17,64 +15,59 @@ server.get("/test", (req, res) => {
   const lastName = req.query.lastName;
   res.end(`your full name is ${firstName} ${lastName}`);
 });
-
 server.get("/user/example", (req, res) => {
   res.json(exampleJson);
 });
-
 server.get("/users", (req, res) => {
-  readJsonData();
-  res.status(200).json(dataInJson);
+  readData();
+  res.status(200).json(data);
 });
-
 server.post("/users", (req, res) => {
-  readJsonData();
-  let incomingData = req.body;
-  incomingData["id"] = uuid.v4();
-  dataInJson.push(incomingData);
-  writeJsonData();
+  readData();
+  let requestData = req.body;
+  requestData["id"] = uuid.v4();
+  data.push(requestData);
+  writeData();
   res.status(200).end("success");
 });
 server.put("/users/:id", (req, res) => {
-  readJsonData();
-  let incomingData = req.body;
+  readData();
+  let requestData = req.body;
   const id = req.params.id;
-  for (let i = 0; i < dataInJson.length; i++) {
-    if (dataInJson[i]["id"] === id) {
-      dataInJson[i]["name"] = incomingData["name"];
-      dataInJson[i]["age"] = incomingData["age"];
-      writeJsonData();
+  for (let i = 0; i < data.length; i++) {
+    if (data[i]["id"] === id) {
+      data[i]["name"] = requestData["name"];
+      data[i]["age"] = requestData["age"];
+      writeData();
       res.status(200).end("User record updated");
     }
   }
   res.status(404).end("No user with that ID");
 });
-
 server.delete("/users/:id", (req, res) => {
   //using url params
-  readJsonData();
+  readData();
   const id = req.params.id;
-  for (let i = 0; i < dataInJson.length; i++) {
-    if (dataInJson[i]["id"] === id) {
-      dataInJson.splice(i, 1);
-      writeJsonData();
+  for (let i = 0; i < data.length; i++) {
+    if (data[i]["id"] === id) {
+      data.splice(i, 1);
+      writeData();
       res.status(200).end("user record deleted");
     }
   }
   res.status(404).end("No user with that ID");
 });
-
 server.listen(3000, () => {
   console.log("Server listening at port 3000");
 });
 
 //function to read from JSON file
-function readJsonData() {
-  dataInJson = fs.readFileSync("./data.json", "utf-8");
-  dataInJson = JSON.parse(dataInJson);
+function readData() {
+  data = fs.readFileSync("./data.json", "utf-8");
+  data = JSON.parse(data);
 }
 
 //function to write to JSON file
-function writeJsonData() {
-  fs.writeFileSync("./data.json", JSON.stringify(dataInJson), "utf-8");
+function writeData() {
+  fs.writeFileSync("./data.json", JSON.stringify(data), "utf-8");
 }
